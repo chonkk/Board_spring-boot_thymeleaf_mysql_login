92e18079782dd46335360f441b5780ec3a20fe7d
천광규 <chonkk@castis.com>
2021-07-12 23:50:48 +0900

    init project

---
+++ src/test/java/com/chonkk/board/BoardSpringBootThymeleafMysqlLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chonkk.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardSpringBootThymeleafMysqlLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
